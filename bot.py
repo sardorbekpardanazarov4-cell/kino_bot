105d140320123a46060ce58b6768ecf66cadf02f
sardorbek09 <sardorbekpardanazarov4@gmail.com>
2025-12-08 20:36:00 +0500

    yangilanishlar qo'shildi

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ CHANNEL_LINKS = [
     ("2-kanal", "https://t.me/+JFubXpf3EY40M2U6"),
     ("3-kanal", "https://t.me/+_PYMzySJjn9jM2U6"),
     ("4-kanal", "https://t.me/+R5IUtNr74rcwY2Y6"),
-    ("5-kanal", "https://t.me/+JFubXpf3EY40M2U6"),
+    ("5-kanal", "https://t.me/kinolar_proo"),
     ("Instagram", "https://www.instagram.com/kinolar_pro1?igsh=MXBjamU2cDZ5OWtzZw==")
 ]
 
